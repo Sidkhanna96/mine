1d2ee91ca4db4e5874828bc43139f5b3105d7932
Siddhant Khanna <skhanna1@cs.ualberta.ca>
2017-11-17 18:19:17 -0700

    Need to print numbers by today

---
+++ Friday/minesweeper.s
@@ -0,0 +1,327 @@
+.text
+hasBomb:
+	addi $sp, $sp, -20
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $s1, 8($sp)
+	sw $s2, 12($sp)
+	sw $s3, 16($sp)
+
+	#5x9
+	#Get the Max Column
+	la $s0, gameCols	#Max Col (addr)
+	lw $s1, 0($s0)		#Max Col(9) (value)
+
+	#Get the row/col values
+	move $s0, $a0 		#Row byte(3)
+							#(Because Gameboard is Stored as byte array)
+	
+	move $s2, $a1			#column byte position(4)
+
+	#Getting the position of the tile
+	mult $s0, $s1		#row*columnSize(3*9)
+	mflo $s0
+	add $s0, $s0, $s2	#row*columnSize + col (3*9+4)
+
+	la $s1, gameBoard 	#Get the address of the board
+	
+	add $s1, $s1, $s0	#Tile position in the board
+	
+	####### Check if Bomb ###########
+	#check if it is a Bomb
+	andi $s3, $s1, 0x01		#getting the first bit
+	beq $s3, 0x01, isBomb	
+	li $v0, 0
+
+	j endHasBomb
+
+	isBomb:
+		li $v0, 1
+	
+		j endHasBomb
+
+	endHasBomb:	
+		lw $ra, 0($sp)
+		lw $s0, 4($sp)
+		lw $s1, 8($sp)
+		lw $s2, 12($sp)
+		lw $s3, 16($sp)
+		addi $sp, $sp, 20
+
+		jr $ra
+
+
+.text
+setBomb:
+	addi $sp, $sp, -20
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $s1, 8($sp)
+	sw $s2, 12($sp)
+	sw $s3, 16($sp)
+
+	#sets the tile to be a bomb
+	#5x9
+	#Get the Max Column
+	la $s0, gameCols	#Max Col (addr)
+	lw $s1, 0($s0)		#Max Col(9) (value)
+
+	#Get the row/col values
+	move $s0, $a0 		#Row byte(3)
+							#(Because Gameboard is Stored as byte array)
+	
+	move $s2, $a1			#column byte position(4)
+
+	#Getting the position of the tile
+	mult $s0, $s1		#row*columnSize(3*9)
+	mflo $s0
+	add $s0, $s0, $s2	#row*columnSize + col (3*9+4)
+
+	la $s1, gameBoard 	#Get the address of the board
+	
+	add $s1, $s1, $s0	#Tile position in the board
+
+	#######Setting Up Bombs ###########
+
+	#setting it to a bomb (unrevealed ?)
+	li $s3, 0x01 		#loading byte to s3
+	sb $s3, 0($s1)		#putting the byte 1 at the position
+
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	lw $s1, 8($sp)
+	lw $s2, 12($sp)
+	lw $s3, 16($sp)
+	addi $sp, $sp, 20
+
+	jr $ra
+
+.text
+printTile:
+	addi $sp, $sp, -24
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $s1, 8($sp)
+	sw $s2, 12($sp)
+	sw $s3, 16($sp)
+	sw $s4, 20($sp)
+
+
+	#5x9
+	#Get the Max Column
+	la $s0, gameCols	#Max Col (addr)
+	lw $s1, 0($s0)		#Max Col(9) (value)
+
+	#Get the row/col values
+	move $s0, $a0		#Row byte(3)
+						#(Because Gameboard is Stored as byte array)
+	
+	move $s2, $a1		#column byte position(4)
+
+	#Getting the position of the tile
+	mult $s0, $s1		#row*columnSize(3*9)
+	mflo $s0
+	add $s0, $s0, $s2	#row*columnSize + col (3*9+4)
+
+	la $s1, gameBoard 	#Get the address of the board
+	
+	add $s1, $s1, $s0	#Tile position in the board
+
+	#####Printing the bombs########
+	lb $s4, 0($s1)
+	beq $s4, 0x01, printBomb
+	sra $s1, $s1, 4
+
+	lb $s4, 0($s1)
+	beq $s4, 0, print0
+	beq $s4, 1, print1
+	# beq $s4, 2, print2
+	# beq $s4, 3, print3
+	# beq $s4, 4, print4
+	# beq $s4, 5, print5
+	# beq $s4, 6, print6
+	# beq $s4, 7, print7
+	# beq $s4, 8, print8
+	sll $s1, $s1, 4
+
+	la $s3, tile
+	move $a2, $a1
+	move $a1, $a0
+	move $a0, $s3
+	
+	jal printString
+	
+	j endPrintTile
+
+	printBomb:
+		la $s3, bomb
+		move $a2, $a1
+		move $a1, $a0
+		move $a0, $s3
+	
+		jal printString
+
+		j endPrintTile
+
+	print0:
+		sll $s1, $s1, 4
+		la $s3, has0
+		move $a2, $a1
+		move $a1, $a0
+		move $a0, $s3
+	
+		jal printString
+
+		j endPrintTile
+
+	print1:
+		sll $s1, $s1, 4
+		la $s3, has1
+		move $a2, $a1
+		move $a1, $a0
+		move $a0, $s3
+	
+		jal printString
+
+		j endPrintTile
+
+	endPrintTile:
+		lw $ra, 0($sp)
+		lw $s0, 4($sp)
+		lw $s1, 8($sp)
+		lw $s2, 12($sp)
+		lw $s3, 16($sp)
+		lw $s4, 20($sp)
+		addi $sp, $sp, 24
+	
+		jr $ra
+
+
+
+.text
+prepareBoard:
+	addi $sp, $sp, -36
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $s1, 8($sp)
+	sw $s2, 12($sp)
+	sw $s3, 16($sp)
+	sw $s4, 20($sp)
+	sw $s5, 24($sp)
+	sw $s6, 28($sp)
+	sw $s7, 32($sp)
+
+
+	la $s0, gameRows	#Total number of Rows(4)
+	lw $s1, 0($s0)
+
+	la $s2, gameCols 	#Total number of Columns(4)
+	lw $s3, 0($s2)
+
+	mult $s3, $s1		#Column*Row (4*4)
+	mflo $s2
+
+	la $s0, gameBoard 	#getting the first position
+
+	loop:
+		beq $s0, $s2, endPrepareBoard
+		lw $s4, 0($s0)	#getting the word at the address of gameboard
+		li $s7, 0
+		bne $s4, 0x01, checkBomb
+		j ContinueLoop 
+
+	ContinueLoop:
+	
+		addi $s0, $s0, 1
+		j loop
+
+	endPrepareBoard:
+		lw $ra, 0($sp)
+		lw $s0, 4($sp)
+		lw $s1, 8($sp)
+		lw $s2, 12($sp)
+		lw $s3, 16($sp)
+		lw $s4, 20($sp)
+		lw $s5, 24($sp)
+		lw $s6, 28($sp)
+		lw $s7, 32($sp)
+		addi $sp, $sp, 36
+
+		jr $ra
+
+	######Checking If Bomb around the number#######
+	checkBomb:
+
+		beq $s7, 0, goBackRow
+		beq $s7, 1, goFrontRow
+		beq $s7, 2, goOriginalPos
+		beq $s7, 3, endCheckBomb
+
+	goBackRow:
+		addi $s7, $s7, 1
+		sub $s0, $s0, $s1
+		j continue
+
+	goFrontRow:
+		addi $s7, $s7, 1
+		add $s0, $s0, $s1
+		add $s0, $s0, $s1
+		j continue
+
+	goOriginalPos:
+		addi $s7, $s7, 1
+		sub $s0, $s0, $s1
+		j continue
+
+
+	continue:
+
+		lw $s5, 0($s0)
+		bne $s5, 0x01, continue1
+
+		sra $s0, $s0, 4
+		addi $s0, $s0, 0x01
+		sll $s0, $s0, 4
+
+	continue1:
+
+		addi $s0, $s0, -1
+		lw $s5, 0($s0)
+		bne $s5, 0x01, continue2
+
+		sra $s0, $s0, 4
+		addi $s0, $s0, 0x01
+		sll $s0, $s0, 4
+
+	continue2:
+
+		addi $s0, $s0, 1
+		addi $s0, $s0, 1
+
+		lw $s5, 0($s0)
+		bne $s5, 0x01, continue3
+
+		sra $s0, $s0, 4
+		addi $s0, $s0, 1
+		sll $s0, $s0, 4
+
+	continue3:
+		
+		addi $s0, $s0, 11
+
+		j checkBomb
+
+
+	endCheckBomb:
+		j ContinueLoop
+
+
+
+
+######USELESS######
+
+
+.data
+.text
+getTile:
+	jr $ra
--- Friday/minesweeper3.s
+++ Friday/minesweeper3.s
@@ -115,3 +115,71 @@ printTile:
 	move $a2, $a1
 	move $a1, $a0
 	
+
+.data
+
+.kdata
+	s1: .word 0
+	s2: .word 0
+
+# modifying our exception Handler here -> determine if keyboard or timer interrupt
+.ktext 0x80000180
+	move $k1, $at		#no idea what it does
+	
+	sw $v0, s1 			# Reloading v0 and a0 registers
+	sw $a0, s2
+
+	mfc0 $k0, $13				#Readinf from cause register to see the cause of problem
+	andi $a0, $k0, 0x0800  		# Get the 11 byte value 
+	srl $a0, $a0, 11			# Get the value stored at 11 to byte 1 position
+	bgtz $a0, Keyboard
+	 			# if value more than zero hence keyboard interuption happened
+
+	mfc0 $k0, $13
+	andi $a0, $k0, 0x8000 		# Get the 15 byte value to check if timer interrupt happening
+	srl $a0, $a0, 15			# shifting the value to the right to get the value at 15 position 
+	bgtz $a0, Timer 			# If value greater than zero then timer interrupt happened
+
+keyboard:
+	lw $s3, 0xffff0004			# Checking what keyboard value was entered and storing it in s3
+	beq $s3, 0x71, quit		# If value equal to 'q' then go to JumpM
+	beq $s3, 0x38, moveUp
+
+	mtc0 $0, $13				# Clearing Cause Register
+	lw $v0, s1 					# Restoring v0 and a0 values
+	lw $a0, s2				
+	
+	eret						# escape to main program
+
+moveUp:
+	la $s1, cursorRow
+	addi $s2, $s1, -1
+	la $s0, newCursorRow
+	sb $s2, 0($s0)
+
+	mtc0 $0, $13 
+	lw $v0, s1
+	lw $a0, s2
+
+	eret
+
+quit:
+	#Later - set the value of the timer to negative to abort the program
+
+Timer:
+	#later
+
+
+.text
+.globl __start
+__start:
+
+	#To enable Status Register interrupt($12) -> keyboard / timer
+	mfc0 $k0, $12   		# enabling Status Register
+	ori $k0, 0x8801 		# initializing the 1st 11th and 15th values
+	mtc0 $k0, $12			# Clearing the Register
+
+	# enable keyboard control
+	lw $s2, 0xffff0000 		# Enabling the Keyboard Control
+	ori $s2, $s2, 0x02
+	sw $s2, 0xffff0000 
